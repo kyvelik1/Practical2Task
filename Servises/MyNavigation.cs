1dbddac1e73a0f94c885dc3bea1c9128710e81c8
kyvelik1 <kyvelik17@gmail.com>
2025-01-23 18:49:30 +0300

    Add files via upload

---
+++ Servises/MyNavigation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfApp4.Servises
+{
+    public static class MyNavigation
+    {
+        public static Frame MyConnect {  get; set; }
+    }
+}
